f87ec1f84553e70235da68dfe37eb8d895cb2a1a
juanmartinez238 <juan.martinez238@yahoo.com>
2021-04-24 23:43:24 -0400

    added functionality to finder page to search for cards using axios get request

--- src/Components/Finder.jsx
+++ src/Components/Finder.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function Finder() {
+
+    const [card, setCard] = useState("");
+    const [data, setData] = useState([]);
+
+    const handleChange = (e) => {
+        
+        console.log(e.target.value)
+        setCard(e.target.value);
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const url = "https://api.pokemontcg.io/v2/cards?q=name:";
+        const response = await axios.get(`${url}${card}`);
+        console.log("this is response", response.data.data);
+        setData(response.data.data);
+    }
     return (
         <div>
-            HELLO FROM FINDER
+            {console.log("this is data", data)}
+            <label>Search for card</label>
+            <input value={card} type="text" placeholder="type card name" onChange={handleChange}/>
+            <button type="submit" onClick={handleSubmit}>Find Card</button>
+            {/* CARD WILL GO HERE */}
+            <div>
+                {data.map((card) => {
+                    console.log("this is card", card.images)
+                    return(
+                        <img src={card.images.small} />
+                    )
+                })}
+            </div>
         </div>
     )
 }
