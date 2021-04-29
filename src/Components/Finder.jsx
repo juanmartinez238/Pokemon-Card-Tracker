e628ecac891e1bce0c896c2f932c4bfd1eb354a7
juanmartinez238 <juan.martinez238@yahoo.com>
2021-04-28 20:33:37 -0400

    made returned cards for finder page more responsive

--- src/Components/Finder.jsx
+++ src/Components/Finder.jsx
@@ -26,17 +26,22 @@ function Finder() {
  
     }
     return (
-        <div>
+        <div className="finder__container">
+            <form onSubmit={handleSubmit}>
             {console.log("this is data", data)}
             <label>Search for card</label>
             <input value={card} type="text" placeholder="type card name" onChange={handleChange}/>
-            <button type="submit" onClick={handleSubmit}>Find Card</button>
+            <button type="submit">Find Card</button>
+            </form>
+            
             {/* CARD WILL GO HERE */}
-            <div>
+            <div className="finder__cardcont">
                 {data.map((card) => {
                     console.log("this is card", card.images)
                     return(
+                        <div className="finder__card">
                         <img src={card.images.small} />
+                        </div>
                     )
                 })}
             </div>
